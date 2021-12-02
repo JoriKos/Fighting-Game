5ec5f8500e232998d4db8f4cb32ffe3470d09f26
Jorikos <26204@ma-web.nl>
2021-12-02 12:15:30 +0100

    Camera clamp, new input system

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private float minBoundaryX, maxBoundaryX, minBoundaryY, maxBoundaryY;
+
+    private void Update()
+    {
+        //Clamps camera between set X and Y positions
+        transform.position = new Vector3(
+                Mathf.Clamp(transform.position.x, minBoundaryX, maxBoundaryX),
+                Mathf.Clamp(transform.position.y, minBoundaryY, maxBoundaryY),
+                transform.position.z
+            );
+    }
+}
