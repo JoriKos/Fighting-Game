77ecba9eda93447f6ac9135564349feb471920d8
Jorikos <26204@ma-web.nl>
2021-12-03 15:16:58 +0100

    log

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,44 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private int movementSpeed, jumpHeight;
+    [SerializeField] private float movementSpeed, jumpHeight, timer;
+    private bool canJump;
 
+    private void Awake()
+    {
+        canJump = true;
+    }
 
+    private void Update()
+    {
+        if (!canJump)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer > 0.75f)
+        {
+            canJump = true;
+            timer = 0f;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.right * movementSpeed * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.left * movementSpeed * Time.deltaTime);
+        }
+
+        if (canJump)
+        {
+            if (Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(Vector3.up * jumpHeight * Time.deltaTime);
+                canJump = false;
+            }
+        }
+    }
 }
