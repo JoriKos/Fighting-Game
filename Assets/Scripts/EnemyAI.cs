fb60a908a5fe14d35c575f60b8a46f33db7cc72f
Jorikos <26204@ma-web.nl>
2021-12-15 12:50:03 +0100

    AI progress

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -4,5 +4,92 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    //State machine. States: Attack, approach, retreat
+    [SerializeField] private float timer, timer2, stateTimer, stateDuration, attackCooldown, attackDuration, playerX;
+    private GameObject player;
+    private CapsuleCollider2D attackCollider;
+    private bool canAttack, timerStart;
+    private int state;
 
+    private void Awake()
+    {
+        player = GameObject.Find("Player");
+        attackCollider = GetComponentInChildren<CapsuleCollider2D>();
+        attackCollider.enabled = false;
+        canAttack = true;
+        state = -1;
+    }
+
+    private void Update()
+    {
+        playerX = player.transform.position.x;
+
+        #region State duration
+        stateTimer += Time.deltaTime;
+        #endregion
+
+        #region Attack duration
+        if (timerStart)
+        {
+            timer += Time.deltaTime;
+        }
+
+        if (timer > attackDuration)
+        {
+            attackCollider.enabled = false;
+            timerStart = false;
+            timer = 0;
+        }
+        #endregion
+
+        #region Attack cooldown
+        if (!canAttack)
+        {
+            timer2 += Time.deltaTime;
+        }
+
+        if (timer2 > attackCooldown)
+        {
+            canAttack = true;
+            timer2 = 0;
+        }
+        #endregion
+
+        switch (state)
+        {
+            case 0:
+                Attack();
+                break;
+            case 1:
+                Approach();
+                break;
+            case 2:
+                Retreat();
+                break;
+        }
+    }
+
+
+    private void Attack()
+    {
+        attackCollider.enabled = true;
+        timerStart = true;
+    }
+
+    private void Approach()
+    {
+        if (playerX + this.transform.position.x > 5)
+        {
+            transform.Translate(Vector2.left);
+        }
+        else
+        {
+            state = 0;
+        }
+    }
+    
+    private void Retreat()
+    {
+
+    }
 }
